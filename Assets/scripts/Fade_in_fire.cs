92b757d213f61422e968c6300b8d06d4901241f2
Phaedra Paul <phaedra@phaedra.net>
2018-11-08 14:24:04 -0500

    backup commit to repo

---
+++ Assets/scripts/Fade_in_fire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade_in_fire : MonoBehaviour {
+
+	public Light fire;
+	void Start () {
+		fire = GetComponent<Light>();
+		fire.intensity = 0.2F;
+		if(!enabled){
+			enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(fire.intensity < 1.3) {
+			fire.intensity += 0.1F;
+		}
+	}
+}
---
+++ Assets/scripts/Flashlight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+	public Light light;
+	void Start () {
+        	Debug.Log(light.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("flashlight_toggle_l") || Input.GetButtonDown("flashlight_toggle_r") || Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Ding motherfucker");
+            light.enabled = !light.enabled;
+        }
+	}
+}
